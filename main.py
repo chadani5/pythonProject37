0cc4ffb783cae3620c1375a1c793063a21fd28c3
chadani5 <np03cs4s230170@heraldcollege.edu.np>
2023-04-05 21:02:42 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+# create a set of integers
+my_set = {1, 2, 3, 4, 5}
+
+# add a new integer to the set
+my_set.add(6)
+
+# print the updated set
+print(my_set)
+
+
+
+
+
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
